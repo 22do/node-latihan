f4e30e85934df17d2e0df9919975d6fed78cbf0a
Doni <donisgemilang@gmail.com>
2021-09-30 22:50:29 +0700

    update

---
+++ max.js
@@ -0,0 +1,18 @@
+function nilaiMaks(x) {
+  i = 0;
+  while (i < x.length) {
+    {
+      if (i == 0) {
+        var nilai_mak = x[i];
+      } else {
+        if (x[i] < nilai_mak) {
+          nilai_mak = x[i];
+        }
+      }
+    }
+    i += 1;
+  }
+  return nilai_mak;
+}
+
+export default nilaiMaks;
