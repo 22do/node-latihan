b0915907cc97e5ce990909d9b7a33282d6607abe
Doni <donisgemilang@gmail.com>
2021-11-12 09:20:38 +0700

    Update

--- ValidateType.js
+++ ValidateType.js
@@ -1,40 +1,46 @@
 let object1 = {
-	"data utama": {
-		"description": "Ini tentang info pokok customer properties",
-		"properties": {
-			"Nama": "text",
-			"Gender": "text",
-			"Umur": "number",
-			"Telp": "telp-number"
-		}
-	}
-}
+  "data utama": {
+    description: "Ini tentang info pokok customer properties",
+    properties: {
+      Nama: "text",
+      Gender: "text",
+      Umur: "number",
+      Telp: "telp-number",
+    },
+  },
+};
 
-function validateType(object1){
-	let hasil = false
-	if(typeof object1 === 'object'){
-		for(let prop in object1){//prop = data utama
-			if(typeof prop === 'string'){
-			//console.log(typeof object1[prop]['description'])
-				let description = object1[prop]['description']
-				if(typeof description === 'string'){
-					for(let properti in object1[prop]['properties']){
-						//console.log(properti) = umur dll
-						let keterangan = object1['data utama']['properties'][properti]
-						//console.log(keterangan) = type dll
-						if(keterangan || 'text' || 'number' || 'telp-number' || 'password'){
-							//console.log(`${keterangan } =`, true)
-							hasil = true
-						}
-            else {
-              hasil = false
+function validateType(object1) {
+  let hasil = false;
+  if (typeof object1 === "object") {
+    for (let prop in object1) {
+      //prop = data utama
+      if (typeof prop === "string") {
+        //console.log(typeof object1[prop]['description'])
+        let description = object1[prop]["description"];
+        if (typeof description === "string") {
+          for (let properti in object1[prop]["properties"]) {
+            //console.log(properti) = umur dll
+            let keterangan = object1["data utama"]["properties"][properti];
+            //console.log(keterangan) = type dll
+            if (
+              keterangan ||
+              "text" ||
+              "number" ||
+              "telp-number" ||
+              "password"
+            ) {
+              //console.log(`${keterangan } =`, true)
+              hasil = true;
+            } else {
+              hasil = false;
             }
-					}
-				}
-			}
-		}
-	}
-	return hasil
+          }
+        }
+      }
+    }
+  }
+  return hasil;
 }
 
-console.log(validateType(object1))
+console.log(validateType(object1));
